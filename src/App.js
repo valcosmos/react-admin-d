10b68b067f0cbeab2d96035b3786d83229e4c8db
Cupid Valentine <westerngod@126.com>
2022-03-29 12:25:46 +0800

    配置路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Layout'
+import Login from './pages/Login'
 function App() {
-  return <div className="App">root</div>
+  return (
+    <Router>
+      <div className="App">
+        {/* <Link to={'/login'}>登陆</Link>
+        <Link to={'/home'}>首页</Link> */}
+        {/* 配置路由的规则 */}
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          {/* 配置404组件 */}
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App
